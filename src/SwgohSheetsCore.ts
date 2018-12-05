745c4b469f0f236a18bcab0be6dd8b0aa9969d70
PopGoesTheWza <PopGoesTheWza@gmail.com>
2018-12-05 20:40:03 +0100

    A whole lotta change

--- src/SwgohSheetsCore.ts
+++ src/SwgohSheetsCore.ts
@@ -71,6 +71,7 @@ interface UnitDefinition {
 
 type Ability = {
   name: string;
+  type: string;
   tier: number;
   isZeta: boolean;
 };
@@ -108,11 +109,86 @@ enum DATASOURCES {
 enum SHEETS {
   SETUP = 'coreSetup',
   MEMBERS = 'coreRoster',
-  UNITS = 'coreUnits',
+  HEROES = 'coreHeroes',
+  SHIPS = 'coreShips',
+  // UNITS = 'coreUnits',
 }
 
 /** settings related functions */
 namespace config {
+
+  /** use members galactic power stats */
+  export function membersGP(): boolean {
+
+    const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
+      .getRange(19, 9)
+      .getValue() as string;
+
+    return result === 'ON';
+  }
+
+  /** use members galactic power stats */
+  export function membersBattles(): boolean {
+
+    const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
+      .getRange(20, 9)
+      .getValue() as string;
+
+    return result === 'ON';
+  }
+
+  /** use members galactic power stats */
+  export function membersGuildActivities(): boolean {
+
+    const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
+      .getRange(21, 9)
+      .getValue() as string;
+
+    return result === 'ON';
+  }
+
+  export enum UNIT_TYPES {
+    BOTH = 'Heroes & Ships',
+    HERO = 'Heroes only',
+    SHIP = 'Ships only',
+  }
+
+  /** use members galactic power stats */
+  export function unitTypes(): string {
+
+    const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
+      .getRange(24, 9)
+      .getValue() as string;
+
+    return result;
+  }
+
+  export enum HERO_ABILITIES {
+    NONE = 'None',
+    ZETAS = 'Zetas only',
+    WORTHY = 'Leader & zetas',
+    DETAILED = 'Detailed',
+  }
+
+  /** use members galactic power stats */
+  export function heroAbilities(): string {
+
+    const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
+      .getRange(25, 9)
+      .getValue() as string;
+
+    return result;
+  }
+
+  /** use members galactic power stats */
+  export function shipAbilities(): boolean {
+
+    const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
+      .getRange(21, 9)
+      .getValue() as string;
+
+    return result === 'ON';
+  }
 
   /** SwgohHelp related settings */
   export namespace SwgohHelp {
@@ -120,10 +196,8 @@ namespace config {
     /** Get the SwgohHelp API username */
     export function username(): string {
 
-      const metaSWGOHLinkCol = 9;
-      const metaSWGOHLinkRow = 15;
       const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
-        .getRange(metaSWGOHLinkRow, metaSWGOHLinkCol)
+        .getRange(15, 9)
         .getValue() as string;
 
       return result;
@@ -132,10 +206,8 @@ namespace config {
     /** Get the SwgohHelp API password */
     export function password(): string {
 
-      const metaSWGOHLinkCol = 9;
-      const metaSWGOHLinkRow = 16;
       const result = SPREADSHEET.getSheetByName(SHEETS.SETUP)
-        .getRange(metaSWGOHLinkRow, metaSWGOHLinkCol)
+        .getRange(16, 9)
         .getValue() as string;
 
       return result;
@@ -155,6 +227,12 @@ namespace Units {
 }
 
 namespace Core {
+
+  enum UnitsAbilities {
+    NONE = 'None',
+    ZETAS_ONLY = 'Zetas only',
+    ALL = 'All',
+  }
 
   export const refreshData = (): void => {
 
@@ -228,6 +306,7 @@ namespace Core {
           }
         }
         if (!found) {
+          // TODO: complete Add feature
           // try to guess data source
           // const setup = settings.find(e => e.guildId === target.id);
           // const g = SwgohGg.getPlayerData(allyCode);
@@ -265,61 +344,102 @@ namespace Core {
   };
 
   const writeGuildNames = (settings: GuildSettings[]): void => {
-    const values: string[][] = [];
+
+    const range = SPREADSHEET.getSheetByName(SHEETS.SETUP).getRange(3, 2, 10, 5);
+    const values = range.getValues() as string[][];
     for (const setting of settings) {
-      values.push([setting.name]);
+      const name = setting.name;
+      const dataSource = setting.dataSource;
+      let row;
+      if (dataSource === DATASOURCES.SWGOH_GG) {
+        const id = setting.guildId;
+        row = values.find(e => e[0] === 'ON' && e[2] === dataSource && +e[3] === id);
+      } else {
+        const id = setting.allyCode;
+        row = values.find(e => e[0] === 'ON' && e[2] === dataSource && +e[4] === id);
+      }
+      if (row && row[1] !== name) {
+        row[1] = name;
+      }
     }
-    SPREADSHEET.getSheetByName(SHEETS.SETUP)
-      .getRange(3, 3, values.length)
-      .setValues(values);
+    range.setValues(values);
   };
 
   const writeRoster = (datas: GuildData[]): void => {
+    const addGP = config.membersGP();
+    const addBattles = config.membersBattles();
+    const addActivities = config.membersGuildActivities();
     const values: any[][] = [];
     for (const data of datas) {
       if (data) {
         const guildId = data.id;
         const guildName = data.name;
         for (const member of data.members) {
-          values.push([
+          let columns = [
             guildId,
             guildName,
             member.name,
             member.allyCode,
             member.level,
-            member.gp,
-            member.heroesGp,
-            member.shipsGp,
-            member.fleetArenaBattlesWon,
-            member.squadArenaBattlesWon,
-            member.normalBattlesWon,
-            member.hardBattlesWon,
-            member.galacticWarBattlesWon,
-            member.guildRaidsWon,
-            member.guildTokensEarned,
-            member.gearDonatedInGuildExchange,
-          ]);
+          ];
+          if (addGP) {
+            columns = columns.concat([
+              member.gp,
+              member.heroesGp,
+              member.shipsGp,
+            ]);
+          }
+          if (addBattles) {
+            columns = columns.concat([
+              member.fleetArenaBattlesWon,
+              member.squadArenaBattlesWon,
+              member.normalBattlesWon,
+              member.hardBattlesWon,
+              member.galacticWarBattlesWon,
+            ]);
+          }
+          if (addActivities) {
+            columns = columns.concat([
+              member.guildRaidsWon,
+              member.guildTokensEarned,
+              member.gearDonatedInGuildExchange,
+            ]);
+          }
+          values.push(columns);
         }
       }
     }
-    const headers = [
+    let headers = [
       'guildId',
       'guildName',
       'name',
       'allyCode',
       'level',
-      'gp',
-      'heroesGp',
-      'shipsGp',
-      'fleetArenaBattlesWon',
-      'squadArenaBattlesWon',
-      'normalBattlesWon',
-      'hardBattlesWon',
-      'galacticWarBattlesWon',
-      'guildRaidsWon',
-      'guildTokensEarned',
-      'gearDonatedInGuildExchange',
     ];
+
+    if (addGP) {
+      headers = headers.concat([
+        'gp',
+        'heroesGp',
+        'shipsGp',
+      ]);
+    }
+    if (addBattles) {
+      headers = headers.concat([
+        'fleetArenaBattlesWon',
+        'squadArenaBattlesWon',
+        'normalBattlesWon',
+        'hardBattlesWon',
+        'galacticWarBattlesWon',
+      ]);
+    }
+    if (addActivities) {
+      headers = headers.concat([
+        'guildRaidsWon',
+        'guildTokensEarned',
+        'gearDonatedInGuildExchange',
+      ]);
+    }
     Sheets.setValues(
       SPREADSHEET.getSheetByName(SHEETS.MEMBERS),
       values,
@@ -328,9 +448,19 @@ namespace Core {
   };
 
   const writeUnits = (datas: GuildData[], baseUnits: UnitDefinition[]): void => {
-    const values: any[][] = [];
-    let nAbilities = 0;
-    let nColumns = 0;
+    // TODO: use distinct sheet for base unit data (tags, abilities...)
+    const unitTypes = config.unitTypes();
+    const addHeroes = unitTypes !== config.UNIT_TYPES.SHIP;
+    const addShips = unitTypes !== config.UNIT_TYPES.HERO;
+    const heroAbilities = config.heroAbilities();
+    const shipAbilities = config.shipAbilities();
+    const heroes: any[][] = [];
+    const ships: any[][] = [];
+    // const values: any[][] = [];
+    const heroesAbilities = { count: 0, columns: 0 };
+    const shipsAbilities = { count: 0, columns: 0 };
+    // let nAbilities = 0;
+    // let nColumns = 0;
     for (const data of datas) {
       if (data) {
         // const guildId = data.id;
@@ -340,55 +470,151 @@ namespace Core {
           for (const baseId in units) {
             const def = baseUnits.find(e => e.baseId === baseId);
             const unit = units[baseId];
-            const columns = [
-              // guildId,
-              // guildName,
-              member.name,
-              member.allyCode,
-              def.name,
-              def.tags,
-              unit.type,
-              unit.rarity,
-              unit.level,
-              unit.gearLevel,
-              unit.power,
-            ];
-            for (const ability of unit.abilities) {
-              columns.push(ability.name);
-              columns.push(ability.isZeta ? 'true' : 'false');
-              columns.push(ability.tier);
+            const type = unit.type;
+            if ((addHeroes && type === Units.TYPES.HERO)
+              || (addShips && type === Units.TYPES.SHIP)) {
+              const columns = type === Units.TYPES.HERO ? [
+                member.name,
+                member.allyCode,
+                def.name,
+                def.tags,
+                unit.rarity,
+                unit.level,
+                unit.gearLevel,
+                unit.power,
+              ] : [
+                member.name,
+                member.allyCode,
+                def.name,
+                def.tags,
+                unit.rarity,
+                unit.level,
+                unit.power,
+              ];
+              if (heroAbilities !== config.HERO_ABILITIES.NONE && type === Units.TYPES.HERO) {
+                // TODO: write abilites (enum UnitsAbilities)
+                if (heroAbilities === config.HERO_ABILITIES.ZETAS) {
+                  let count = 0;
+                  for (const ability of unit.abilities) {
+                    if (ability.isZeta && ability.tier === 8) {
+                      columns.push(ability.name);
+                      columns.push(ability.type);
+                      count += 1;
+                    }
+                  }
+                  heroesAbilities.count = Math.max(count, heroesAbilities.count);
+                } else if (heroAbilities === config.HERO_ABILITIES.WORTHY) {
+                  let count = 0;
+                  for (const ability of unit.abilities) {
+                    if (ability.type === 'leaderskill' || (ability.isZeta && ability.tier === 8)) {
+                      columns.push(ability.name);
+                      columns.push(ability.type);
+                      columns.push(ability.isZeta ? 'true' : 'false');
+                      columns.push(ability.tier);
+                      count += 1;
+                    }
+                  }
+                  heroesAbilities.count = Math.max(count, heroesAbilities.count);
+                } else if (heroAbilities === config.HERO_ABILITIES.DETAILED) {
+                  for (const ability of unit.abilities) {
+                    columns.push(ability.name);
+                    columns.push(ability.type);
+                    columns.push(ability.isZeta ? 'true' : 'false');
+                    columns.push(ability.tier);
+                  }
+                  heroesAbilities.count = Math.max(unit.abilities.length, heroesAbilities.count);
+                }
+                heroesAbilities.columns = Math.max(columns.length, heroesAbilities.columns);
+                // nAbilities = Math.max(unit.abilities.length, nAbilities);
+                // nColumns = Math.max(columns.length, nColumns);
+              } else if (shipAbilities && type === Units.TYPES.SHIP) {
+                // TODO: write abilites (enum UnitsAbilities)
+                for (const ability of unit.abilities) {
+                  columns.push(ability.name);
+                  columns.push(ability.type);
+                  columns.push(ability.tier);
+                }
+                shipsAbilities.count = Math.max(unit.abilities.length, shipsAbilities.count);
+                shipsAbilities.columns = Math.max(columns.length, shipsAbilities.columns);
+                // nAbilities = Math.max(unit.abilities.length, nAbilities);
+                // nColumns = Math.max(columns.length, nColumns);
+              }
+              (type === Units.TYPES.HERO ? heroes : ships).push(columns);
+              // values.push(columns);
             }
-            nAbilities = Math.max(unit.abilities.length, nAbilities);
-            nColumns = Math.max(columns.length, nColumns);
-            values.push(columns);
           }
         }
       }
     }
-    const headers = [
-      // 'guildId',
-      // 'guildName',
+    const heroesHeaders = [
       'name',
       'allyCode',
       'unit',
       'tags',
-      'type',
       'rarity',
       'level',
       'gearLevel',
       'power',
     ];
-    for (let i = 0; i < nAbilities; i += 1) {
-      headers.push(`ability${i}`);
-      headers.push(`isZeta${i}`);
-      headers.push(`tier${i}`);
+    const shipsHeaders = [
+      'name',
+      'allyCode',
+      'unit',
+      'tags',
+      'rarity',
+      'level',
+      'power',
+    ];
+    // TODO: write abilites (enum UnitsAbilities)
+    if (heroAbilities === config.HERO_ABILITIES.ZETAS) {
+      for (let i = 0; i < heroesAbilities.count; i += 1) {
+        heroesHeaders.push(`ability_${i}`);
+        heroesHeaders.push(`type_${i}`);
+      }
+    } else if (heroAbilities === config.HERO_ABILITIES.WORTHY
+      || heroAbilities === config.HERO_ABILITIES.DETAILED) {
+      for (let i = 0; i < heroesAbilities.count; i += 1) {
+        heroesHeaders.push(`ability_${i}`);
+        heroesHeaders.push(`type_${i}`);
+        heroesHeaders.push(`isZeta_${i}`);
+        heroesHeaders.push(`tier_${i}`);
+      }
     }
+    if (shipAbilities) {
+      for (let i = 0; i < shipsAbilities.count; i += 1) {
+        shipsHeaders.push(`ability_${i}`);
+        shipsHeaders.push(`type_${i}`);
+        shipsHeaders.push(`tier_${i}`);
+      }
+    }
+    // for (let i = 0; i < nAbilities; i += 1) {
+    //   headers.push(`ability_${i}`);
+    //   headers.push(`type_${i}`);
+    //   headers.push(`isZeta_${i}`);
+    //   headers.push(`tier_${i}`);
+    // }
     Sheets.setValues(
-      SPREADSHEET.getSheetByName(SHEETS.UNITS),
-      values.map(e =>
-        e.length !== nColumns ? e.concat(Array(nColumns).fill(null)).slice(0, nColumns) : e),
-      headers,
+      SPREADSHEET.getSheetByName(SHEETS.HEROES),
+      heroes.map(e =>
+        e.length !== heroesAbilities.columns
+          ? e.concat(Array(heroesAbilities.columns).fill(null)).slice(0, heroesAbilities.columns)
+          : e),
+      heroesHeaders,
     );
+    Sheets.setValues(
+      SPREADSHEET.getSheetByName(SHEETS.SHIPS),
+      ships.map(e =>
+        e.length !== shipsAbilities.columns
+          ? e.concat(Array(shipsAbilities.columns).fill(null)).slice(0, shipsAbilities.columns)
+          : e),
+      shipsHeaders,
+    );
+    // Sheets.setValues(
+    //   SPREADSHEET.getSheetByName(SHEETS.UNITS),
+    //   values.map(e =>
+    //     e.length !== nColumns ? e.concat(Array(nColumns).fill(null)).slice(0, nColumns) : e),
+    //   headers,
+    // );
   };
 
   type GuildSettings = {
@@ -483,8 +709,11 @@ namespace Core {
   /** compute hash for SwgohHelp credential settings */
   const getCredentialHash = (): string => {
 
-    const credential = SPREADSHEET.getSheetByName(SHEETS.SETUP)
-      .getRange(15, 9, 2).getValues();
+    const sheet = SPREADSHEET.getSheetByName(SHEETS.SETUP);
+
+    const credential = sheet.getRange(15, 9, 2).getValues().concat(
+      sheet.getRange(19, 9, 3).getValues(),
+      sheet.getRange(24, 9, 3).getValues());
 
     return String(Utilities.computeDigest(
       Utilities.DigestAlgorithm.SHA_256,
@@ -608,8 +837,8 @@ namespace Core {
         sheet.deleteRows(lastRow + 1, maxRows - lastRow);
       }
 
-      const lastCol = sheet.getLastRow();
-      const maxcols = sheet.getMaxRows();
+      const lastCol = sheet.getLastColumn();
+      const maxcols = sheet.getMaxColumns();
       if (maxcols > lastCol) {
         sheet.deleteColumns(lastCol + 1, maxcols - lastCol);
       }
